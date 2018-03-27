c466ef440e395e071cd4f2aa11334d4a96969dbc
Nicole I Stein <ns60549@srcs.loc>
2018-03-27 11:54:07 -0700

    changes

--- mathApp.js
+++ mathApp.js
@@ -1,13 +1,24 @@
 var button = document.getElementById('solve');
-button1.addEventListener("click", );
+button.addEventListener('click', () => {
+    document.getElementById('solutions').textContent = solveQ();
+});
 
-function solve() {
+function changeElementClass(id, className) {
+    var el = document.getElementById(id);
+    el.className = className;
+}
+
+function solveQ() {
     var a = document.getElementById('a').value;
     var b = document.getElementById('b').value;
     var c = document.getElementById('c').value;
     var formula1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
     var formula2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-     if (isNaN(a)) || (isNaN(b)) || (isNaN(c)) == true {
-        message.textContent = "The value " + input + " is not a number.";
-        message.style.color = "red";
+    if (isNaN(a))||(isNaN(b))||(isNaN(c)) == true {
+        changeElementClass("solutions", "invalid");
+        return "You entered an incorrect value for either A, B, or C. Please try again.";
+    } else if {
+        changeElementClass("solutions", "real");
+        return "The roots to the quadratic are " + formula1 + " and " + formula2;
+    }
 }
